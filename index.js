83c4f1425f98add02f8080f829936afbc8480aaa
Jitendra saini <151595469+Jitendra064@users.noreply.github.com>
2024-01-31 16:06:54 +0530

    jitendraWeb

---
+++ index.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multitask", {
+  strings: ["React developer", "Web deployment", " Web designer"],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  startDelay: 1000,
+  backDelay: 1500,
+});
